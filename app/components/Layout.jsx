1f25ba64da12e0f1550ec72830187b81b623f91e
JPychon <64781464+JPychon@users.noreply.github.com>
2023-04-06 18:19:31 -0700

    Added the banner component to display the warning. Added the website footer with the logo & contact/resources links. Changed some elements within the layout Updated the logo.

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -2,7 +2,10 @@
 import {Suspense, useEffect} from 'react';
 import {Await, useMatches, useFetchers} from '@remix-run/react';
 import {CartLineItems, CartActions, CartSummary} from '~/components/Cart';
+import {Banner} from '~/components/Banner';
+import {Footer} from '~/components/Footer';
 import {Drawer, useDrawer} from '~/components/Drawer';
+import {Image} from '@shopify/hydrogen';
 
 function CartHeader({cart, openDrawer}) {
 	return (
@@ -10,7 +13,7 @@ function CartHeader({cart, openDrawer}) {
 		<Await resolve={cart}>
 		  {(data) => (
 			<button
-			  className="relative ml-auto flex items-center justify-center w-8 h-8"
+			  className="relative ml-auto flex items-center justify-center w-120 h-8"
 			  onClick={openDrawer}
 			>
 			  <svg
@@ -19,7 +22,7 @@ function CartHeader({cart, openDrawer}) {
 				fill="currentColor"
 				className="w-5 h-5"
 			  >
-				<title>Bag</title>
+				<title>Cart</title>
 				<path
 				  fillRule="evenodd"
 				  d="M8.125 5a1.875 1.875 0 0 1 3.75 0v.375h-3.75V5Zm-1.25.375V5a3.125 3.125 0 1 1 6.25 0v.375h3.5V15A2.625 2.625 0 0 1 14 17.625H6A2.625 2.625 0 0 1 3.375 15V5.375h3.5ZM4.625 15V6.625h10.75V15c0 .76-.616 1.375-1.375 1.375H6c-.76 0-1.375-.616-1.375-1.375Z"
@@ -35,89 +38,99 @@ function CartHeader({cart, openDrawer}) {
 		</Await>
 	  </Suspense>
 	);
-  }
+}
 
-  export function Layout({children, title}) {
-	const {isOpen, openDrawer, closeDrawer} = useDrawer();
-	const fetchers = useFetchers();
-	const [root] = useMatches();
-	const cart = root.data?.cart;
+export function Layout({children, title}) {
+const {isOpen, openDrawer, closeDrawer} = useDrawer();
+const fetchers = useFetchers();
+const [root] = useMatches();
+const cart = root.data?.cart;
 
-	// Grab all the fetchers that are adding to cart
-	const addToCartFetchers = [];
+// Grab all the fetchers that are adding to cart
+const addToCartFetchers = [];
 	for (const fetcher of fetchers) {
 	if (fetcher?.submission?.formData?.get('cartAction') === 'ADD_TO_CART') {
 		addToCartFetchers.push(fetcher);
-	}
+		}
 	}
 	// When the fetchers array changes, open the drawer if there is an add to cart action
 	useEffect(() => {
-	if (isOpen || addToCartFetchers.length === 0) return;
-	openDrawer();
+		if (isOpen || addToCartFetchers.length === 0) return;
+		openDrawer();
 	}, [addToCartFetchers]);
 
-	return (
-	  <div className="flex flex-col min-h-screen antialiased bg-neutral-50">
-		<header
-		  role="banner"
-		  className={`flex items-center h-16 p-6 md:p-8 lg:p-12 sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 antialiased transition shadow-sm`}
-		>
-		  <div className="flex gap-12 w-full items-center">
-			<a className="font-bold" href="/">
-			  {title}
-			</a>
-			<CartHeader cart={cart} openDrawer={openDrawer} />
-		  </div>
-		</header>
-		<main
-		  role="main"
-		  id="mainContent"
-		  className="flex-grow p-6 md:p-8 lg:p-12"
-		>
-		  {children}
-		</main>
-		<Drawer open={isOpen} onClose={closeDrawer}>
-			<CartDrawer cart={cart} close={closeDrawer} />
-		</Drawer>
-	  </div>
-	);
-  }
+return (
+	<div className="flex flex-col min-h-screen antialiased bg-neutral-50">
+	<Banner 
+		title="WARNING:"
+		description="These products contain nicotine - Nicotine is an addictive chemical."
+	/>
+	<header
+		role="banner"
+		className={`flex items-center h-16 p-6 md:p-8 lg:p-12 sticky z-40 top-0 justify-between w-full leading-none gap-4 antialiased transition shadow-md`}
+	>
+		<div className="flex gap-12 w-full items-center mt-7">
+		<a className="font-bold" href="/">
+		<img
+		// Website Logo
+		src="https://i.ibb.co/kxZhCWz/disposables-plug.png"
+		width={200}
+		height={50}
+		/>
+		</a>
+		<CartHeader cart={cart} openDrawer={openDrawer} />
+		</div>
+	</header>
+	<main
+		role="main"
+		id="mainContent"
+		className="flex-grow p-6 md:p-8 lg:p-12"
+	>
+		{children}
+	</main>
+	<Footer/>
+	<Drawer open={isOpen} onClose={closeDrawer}>
+		<CartDrawer cart={cart} close={closeDrawer} />
+	</Drawer>
+	</div>
+  );
+}
 
-  function CartDrawer({cart, close}) {
-	return (
-	  <Suspense>
-		<Await resolve={cart}>
-		  {(data) => (
+function CartDrawer({cart, close}) {
+return (
+	<Suspense>
+	<Await resolve={cart}>
+		{(data) => (
+		<>
+			{data?.totalQuantity > 0 ? (
 			<>
-			  {data?.totalQuantity > 0 ? (
-				<>
-				  <div className="flex-1 overflow-y-auto">
-					<div className="flex flex-col space-y-7 justify-between items-center md:py-8 md:px-12 px-4 py-6">
-					  <CartLineItems linesObj={data.lines} />
-					</div>
-				  </div>
-				  <div className="w-full md:px-12 px-4 py-6 space-y-6 border border-1 border-gray-00">
-					<CartSummary cost={data.cost} />
-					<CartActions checkoutUrl={data.checkoutUrl} />
-				  </div>
-				</>
-			  ) : (
-				<div className="flex flex-col space-y-7 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
-				  <h2 className="whitespace-pre-wrap max-w-prose font-bold text-4xl">
-					Your cart is empty
-				  </h2>
-				  <button
-					onClick={close}
-					className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none bg-black text-white w-full"
-				  >
-					Continue shopping
-				  </button>
+				<div className="flex-1 overflow-y-auto">
+				<div className="flex flex-col space-y-7 justify-between items-center md:py-8 md:px-12 px-4 py-6">
+					<CartLineItems linesObj={data.lines} />
 				</div>
-			  )}
+				</div>
+				<div className="w-full md:px-12 px-4 py-6 space-y-6 border border-1 border-gray-00">
+				<CartSummary cost={data.cost} />
+				<CartActions checkoutUrl={data.checkoutUrl} />
+				</div>
 			</>
-		  )}
-		</Await>
-	  </Suspense>
-	);
-  }
+			) : (
+			<div className="flex flex-col space-y-7 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
+				<h2 className="whitespace-pre-wrap max-w-prose font-bold text-4xl">
+				Your cart is empty
+				</h2>
+				<button
+				onClick={close}
+				className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none bg-black text-white w-full"
+				>
+				Continue shopping
+				</button>
+			</div>
+			)}
+		</>
+		)}
+	</Await>
+	</Suspense>
+  );
+}
   
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -18,13 +18,13 @@ export default function Index() {
   return (
     <section className="w-full gap-4">
       <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
-        {/*rands*/}
+        {/* Collections Title */}
       </h2>
       <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 false  sm:grid-cols-3 false false">
         {collections.nodes.map((collection) => {
           return (
             <Link to={`/collections/${collection.handle}`} key={collection.id}>
-              <div className="grid gap-4">
+              <div className="grid gap-4 border-2">
                 {collection?.image && (
                   <Image
                     alt={`Image of ${collection.title}`}
@@ -38,7 +38,7 @@ export default function Index() {
                     }}
                   />
                 )}
-                <h2 className="whitespace-pre-wrap max-w-prose font-medium text-copy">
+                <h2 className="whitespace-pre-wrap max-w-prose font-bold text-copy text-center">
                   {collection.title}
                 </h2>
               </div>
