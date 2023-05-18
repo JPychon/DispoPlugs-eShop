85280e048d78426dc26e2530c790748ed2c375df
JPychon <64781464+JPychon@users.noreply.github.com>
2023-05-18 00:48:58 -0700

    Updated the slide-show images aspect ratio

--- app/components/SlideShow.jsx
+++ app/components/SlideShow.jsx
@@ -5,17 +5,18 @@ import {Carousel} from 'flowbite-react';
 
 export function SlideShow({collections}) {
     return(
-      <div className="h-52 sm:h-64 xl:h-96 2xl:h-96 ">
+      <div className="h-52 sm:h-64 xl:h-96 2xl:h-96">
         <Carousel slideInterval={5000}>
           {collections.map((collection) => ( // map through the collections array
             <Link key={collection.id} to={`/collections/${collection.handle}`} className="overscroll-x-hidden overflow-hidden">
                   <Image
-                    className="h-52 sm:h-64 xl:h-96 2xl:h-96 aspect-auto"
+                    className="h-52 sm:h-64 xl:h-96 2xl:h-96"
+                    aspectRatio="4/5"
                     width="100%"
                     alt={`Image of ${collection.title}`}
                     data={collection.image}
                     loaderOptions={{
-                      scale: 2,
+                      scale: 3,
                       crop: 'center',
                     }}
                   />
