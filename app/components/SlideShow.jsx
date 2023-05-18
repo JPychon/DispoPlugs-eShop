436f319f04713664e4811d81fdd5492587ca0276
JPychon <64781464+JPychon@users.noreply.github.com>
2023-05-18 00:59:49 -0700

    Updated the slide-show

--- app/components/SlideShow.jsx
+++ app/components/SlideShow.jsx
@@ -5,18 +5,17 @@ import {Carousel} from 'flowbite-react';
 
 export function SlideShow({collections}) {
     return(
-      <div className="h-52 sm:h-64 xl:h-96 2xl:h-96">
+      <div className="h-96 sm:h-64 xl:h-96 2xl:h-96">
         <Carousel slideInterval={5000}>
           {collections.map((collection) => ( // map through the collections array
             <Link key={collection.id} to={`/collections/${collection.handle}`} className="overscroll-x-hidden overflow-hidden">
                   <Image
-                    className="h-52 sm:h-64 xl:h-96 2xl:h-96"
-                    aspectRatio="4/5"
                     width="100%"
+                    className='object-cover h-auto'
                     alt={`Image of ${collection.title}`}
                     data={collection.image}
                     loaderOptions={{
-                      scale: 3,
+                      scale: 2,
                       crop: 'center',
                     }}
                   />
