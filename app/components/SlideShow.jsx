fbdf5496bac52f41c33647c47bac4793f629bb21
JPychon <64781464+JPychon@users.noreply.github.com>
2023-05-17 23:37:01 -0700

    Fixed the image sizing for the slide-show

--- app/components/SlideShow.jsx
+++ app/components/SlideShow.jsx
@@ -10,6 +10,9 @@ export function SlideShow({collections}) {
           {collections.map((collection) => ( // map through the collections array
             <Link key={collection.id} to={`/collections/${collection.handle}`} className="overscroll-x-hidden overflow-hidden">
                   <Image
+                    width="100%"
+                    height="100%"
+                    aspectRatio="4/5"
                     alt={`Image of ${collection.title}`}
                     data={collection.image}
                     loaderOptions={{
