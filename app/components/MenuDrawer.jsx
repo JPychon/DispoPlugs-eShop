370f815b25b087ffa2cdafb0a822b53aa962d49f
JPychon <64781464+JPychon@users.noreply.github.com>
2023-05-21 01:26:12 -0700

    Removed the drawer header

--- app/components/MenuDrawer.jsx
+++ app/components/MenuDrawer.jsx
@@ -2,7 +2,7 @@ import {Drawer, MenuMobileNav} from '~/components';
 
 export function MenuDrawer({isOpen, onClose, menu}) {
     return (
-      <Drawer open={isOpen} onClose={onClose} openFrom="left" heading="Pages">
+      <Drawer open={isOpen} onClose={onClose} openFrom="left" heading="">
         <div className="grid">
           <MenuMobileNav menu={menu} onClose={onClose} />
         </div>
